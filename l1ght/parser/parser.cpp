fed6943710541ab361b9e4f00ee5397e7564c7ab
mirit <mirit6873079@gmail.com>
2020-01-19 23:04:03 +0200

    CopyFromFile fixed

--- l1ght/parser/parser.cpp
+++ l1ght/parser/parser.cpp
@@ -16,45 +16,36 @@ using namespace std;
 
 void Parser::Parse(const char fileName_[])
 {
-    //CopyFromFileToBuff(fileName_);
-    string buff = "one zero + seven eight";
-    string delimiter = " ";
-    size_t pos = 0;
-    std::string token;
-    while ((pos = buff.find(delimiter)) != string::npos)
-    {
-        token = buff.substr(0, pos);
-        std::cout << token << std::endl;
-        //check if itts a num or operator
-        //push it in the right stack
-        buff.erase(0, pos + delimiter.length());
-    }
-    cout << "my buff: ";
-    cout << buff << endl;
+    CopyFromFileToBuff(fileName_);
+    // string buff = "one zero + seven eight";
+    // string delimiter = " ";
+    // size_t pos = 0;
+    // std::string token;
+    // while ((pos = buff.find(delimiter)) != string::npos)
+    // {
+    //     token = buff.substr(0, pos);
+    //     std::cout << token << std::endl;
+    //     // check if itts a num or operator
+    //     // push it in the right stack
+    //     buff.erase(0, pos + delimiter.length());
+    // }
+    // cout << "my buff: ";
+    // cout << buff << endl;
 
 }
 
 string Parser::CopyFromFileToBuff(const char fileName_[])
 {
-    ifstream myfile(fileName_, ios::binary);
+    stringbuf buff;
+    ifstream myFile;
+    myFile.open(fileName_);
+    myFile >> &buff;
 
-    if (!(myfile.is_open()))
-    {
-        throw FileDoesntOpen(std::string("Unable to open file") +
-                                            std::string(strerror(errno)));
-    }
-    string buff;
+    string str;
+    str = buff.str();
+    cout << str;
 
-    // char *token = strtok(ss, " ");
-    // while(nullptr != token)
-    // {
-    //     cout << token << endl;
-    //     token = strtok(nullptr," ");
-    // }
-
-    myfile.close();
-
-    return buff;
+    myFile.close();
 }
 
 FileDoesntOpen::FileDoesntOpen(const std::string &what_)
